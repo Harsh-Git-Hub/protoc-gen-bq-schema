215aa43cfceea7a875a93ed984292d687e6b81c1
Harsh-Git-Hub <hgupta@bolt.com>
2022-02-02 15:59:58 -0800

    Added PolicyTag definitions

--- main.go
+++ main.go
@@ -57,11 +57,17 @@ var (
 
 // Field describes the schema of a field in BigQuery.
 type Field struct {
-	Name        string   `json:"name"`
-	Type        string   `json:"type"`
-	Mode        string   `json:"mode"`
-	Description string   `json:"description,omitempty"`
-	Fields      []*Field `json:"fields,omitempty"`
+	Name        string      `json:"name"`
+	Type        string      `json:"type"`
+	Mode        string      `json:"mode"`
+	Description string      `json:"description,omitempty"`
+	Fields      []*Field    `json:"fields,omitempty"`
+	PolicyTags  *PolicyTags `json:"policyTags,omitempty"`
+}
+
+// PolicyTags define the schema of a policyTag in BigQuery
+type PolicyTags struct {
+	Names []string `json:"names,omitempty"`
 }
 
 // ProtoPackage describes a package of Protobuf, which is an container of message types.
@@ -273,6 +279,10 @@ func convertField(
 
 		if len(opt.Description) > 0 {
 			field.Description = opt.Description
+		}
+
+		if len(opt.PolicyTags) > 0 {
+			field.PolicyTags = &PolicyTags{Names: []string{opt.PolicyTags}}
 		}
 	}
 
